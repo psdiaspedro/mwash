0abcebefa660ce60f61684e09383eee0cfe69d5d
Pedro Dias <psdias.pedro@gmail.com>
2022-12-28 20:28:52 -0300

    v0.6

--- api/src/repositorios/agendamento.go
+++ api/src/repositorios/agendamento.go
@@ -3,7 +3,8 @@ package repositorios
 import (
 	"api/src/models"
 	"database/sql"
-	"errors"
+	// "errors"
+	// "fmt"
 )
 
 type Agendamento struct {
@@ -95,87 +96,31 @@ func (repo Agendamento) BuscarAgendamentosPropriedade(propriedadeID uint64) ([]m
 }
 
 func (repo Agendamento) BuscarAgendamentosPorData(data models.Data) ([]models.Agendamento, error) {
-	if data.Dia == "" && data.Mes == "" && data.Ano != "" {
-		linhas, erro := repo.db.Query("select * from agendamentos where extract(year from dia_agendamento) = ? order by dia_agendamento asc", data.Ano)
-		if erro != nil {
-			return nil, erro
-		}
-		defer linhas.Close()
-
-		var agendamentos []models.Agendamento
-
-		for linhas.Next() {
-			var agendamento models.Agendamento
-
-			if erro = linhas.Scan(
-				&agendamento.ID,
-				&agendamento.PropriedadeID,
-				&agendamento.DiaAgendamento,
-				&agendamento.Checkin,
-				&agendamento.Checkout,
-				&agendamento.Obs,
-			); erro != nil {
-				return nil, erro
-			}
-
-			agendamentos = append(agendamentos, agendamento)
-		}
-		
-		return agendamentos, nil
-	} else if data.Dia == "" && data.Mes != "" && data.Ano != "" {
-		linhas, erro := repo.db.Query("select * from agendamentos where extract(year from dia_agendamento) = ? and extract(month from dia_agendamento) = ? order by dia_agendamento asc", data.Ano, data.Mes)
-		if erro != nil {
-			return nil, erro
-		}
-		defer linhas.Close()
-
-		var agendamentos []models.Agendamento
-
-		for linhas.Next() {
-			var agendamento models.Agendamento
-
-			if erro = linhas.Scan(
-				&agendamento.ID,
-				&agendamento.PropriedadeID,
-				&agendamento.DiaAgendamento,
-				&agendamento.Checkin,
-				&agendamento.Checkout,
-				&agendamento.Obs,
-			); erro != nil {
-				return nil, erro
-			}
-
-			agendamentos = append(agendamentos, agendamento)
-		}
-		
-		return agendamentos, nil
-	} else if data.Dia != "" && data.Mes != "" && data.Ano != "" {
-		linhas, erro := repo.db.Query("select * from agendamentos where extract(year from dia_agendamento) = ? and extract(month from dia_agendamento) = ? and extract(day from dia_agendamento) = ? order by dia_agendamento asc", data.Ano, data.Mes, data.Dia)
-		if erro != nil {
-			return nil, erro
-		}
-		defer linhas.Close()
-
-		var agendamentos []models.Agendamento
-
-		for linhas.Next() {
-			var agendamento models.Agendamento
-
-			if erro = linhas.Scan(
-				&agendamento.ID,
-				&agendamento.PropriedadeID,
-				&agendamento.DiaAgendamento,
-				&agendamento.Checkin,
-				&agendamento.Checkout,
-				&agendamento.Obs,
-			); erro != nil {
-				return nil, erro
-			}
-
-			agendamentos = append(agendamentos, agendamento)
-		}
-		
-		return agendamentos, nil
+	query, valores := data.GerarQueryString(data)
+	linhas, erro := repo.db.Query(query, valores...)
+	if erro != nil {
+		return nil, erro
 	}
-	return nil, errors.New("alguma erro ocorreu")
+	defer linhas.Close()
+
+	var agendamentos []models.Agendamento
+
+	for linhas.Next() {
+		var agendamento models.Agendamento
+
+		if erro = linhas.Scan(
+			&agendamento.ID,
+			&agendamento.PropriedadeID,
+			&agendamento.DiaAgendamento,
+			&agendamento.Checkin,
+			&agendamento.Checkout,
+			&agendamento.Obs,
+		); erro != nil {
+			return nil, erro
+		}
+
+		agendamentos = append(agendamentos, agendamento)
+	}
+		
+	return agendamentos, nil
 }
